e761b34a3f3cdb1094d4c90d5e1efc047f994198
khachikkar <khachikkarapetyan78@gmail.com>
2024-12-05 20:21:56 +0400

    replaced Image component with Image from ant and call now button

--- src/components/productMachine/index.tsx
+++ src/components/productMachine/index.tsx
@@ -1,5 +1,6 @@
 
 import "./index.css"
+import {Button, Image} from "antd";
 interface ProductMachineProps {
     item: {
         id: number;
@@ -12,16 +13,23 @@ interface ProductMachineProps {
 
 const ProductMachine: React.FC<ProductMachineProps> = ({ item }) => {
 
+    const handleClick = () => {
+        window.location.href = `tel:077341019`;
+    };
 
     const image = JSON.parse(item.image_url).publicUrl
 
 
     return (
         <div className="product-card">
-            <img alt={image} className="product-card__image" src={image} />
-            <p className="product-card__brand">{item.name}</p>
-            <p className="product-card__description">{item.description}</p>
-            <p className="product-card__price">{item.price} Դրամ</p>
+            {/*<img alt={image} className="product-card__image" src={image} />*/}
+            <Image alt={image} src={image} style={{height:"300px",width:"100%"}}  />
+            <div>
+                <p className="product-card__brand">{item.name}</p>
+                <p className="product-card__description">{item.description}</p>
+                <p className="product-card__price">{item.price} Դրամ</p>
+                <Button onClick={handleClick}>Զանգել Հիմա</Button>
+            </div>
         </div>
     );
 };
